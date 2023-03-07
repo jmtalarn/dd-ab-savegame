df30bfd5c5ea86d77432056a2a278bad97aada08
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2023-03-07 16:49:35 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,51 @@
+//import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config'
+import macrosPlugin from 'vite-plugin-babel-macros';
+import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react-swc';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default defineConfig({
+  plugins: [
+    react(),
+    macrosPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'D&D Adventure begins - save game utility',
+        short_name: 'dd-ab-savegame',
+        description: 'Utility to save game status for D&D Adventure begins board game.',
+        theme_color: '#0f4d92',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'android-chrome-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+});
