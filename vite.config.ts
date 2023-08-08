0e862eda7337753f014360c19b5d560faec8d2b1
jmtalarn <jmtalarn@gmail.com>
2023-08-08 18:07:31 +0200

    Reconfigured

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -39,6 +39,5 @@ export default defineConfig({
           },
         ],
       },
-    })
-  ],
+    })],
 })
